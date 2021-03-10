a2835546c66fdd66e8b14cd2b6103740939b63fa
gabrielle-panchev <gabriellepanchev@mail.weberedu>
2021-03-09 17:29:43 -0700

    Student Balance Page

--- Assignment1/Controllers/PaymentController.cs
+++ Assignment1/Controllers/PaymentController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Assignment1.Models;
 namespace Assignment1.Controllers
 {
     public class PaymentController : Controller
     {
+        LMS_GRINDEntities1 gds = new LMS_GRINDEntities1();
+
         // GET: Payment
         public ActionResult Balance()
         {
-            return View("BalanceView");
+            var semesterCredits = (from c in gds.Courses
+                                join sc in gds.StudentCourses
+                                on c.course_id equals sc.course_id
+                                where sc.student_id == Name.user_id
+                                select c).ToList();
+
+
+            return View("BalanceView", semesterCredits);
         }
         
         public ActionResult PaymentInfo()
