d5a8b5dd4007c511fdca9296ae408eab002aeb95
gabrielle-panchev <gabriellepanchev@mail.weberedu>
2021-03-25 19:54:22 -0600

    submit assignment and todo list

---
+++ Assignment1/Models/ToDo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment1.Models
+{
+    public class ToDo
+    {
+        public int AssignmentID;
+        public string CourseNum;
+        public string AssignmentName;
+        public Nullable<System.DateTime> DueDate;
+    }
+}
